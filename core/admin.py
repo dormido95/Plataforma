41457286ce0494bf968324e964d299c2fdc475a4
dormido95 <luna.emiliano95@hotmail.com>
2026-01-14 23:08:54 -0300

    Origin main

--- core/admin.py
+++ core/admin.py
@@ -4,3 +4,31 @@ from .models import CreatorProfile, Content, Subscription
 admin.site.register(CreatorProfile)
 admin.site.register(Content)
 admin.site.register(Subscription)
+
+
+@admin.register(CreatorProfile)
+class CreatorProfileAdmin(admin.ModelAdmin):
+ 
+    list_display = ('user', 'subscription_price', 'bio_short')
+    
+  
+    def bio_short(self, obj):
+        return obj.bio[:50] + "..." 
+    bio_short.short_description = 'Resumen de Bio'
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('subscriber', 'creator', 'end_date')
+    
+
+    search_fields = ('subscriber__username', 'creator__user__username')
+    
+
+    list_filter = ('end_date',)
+
+
+@admin.register(Content)
+class ContentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'creator', 'created_at')
+  
+    readonly_fields = ('created_at',)
